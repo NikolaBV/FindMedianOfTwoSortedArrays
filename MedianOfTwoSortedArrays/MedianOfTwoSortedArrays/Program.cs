5b026b48d9bc7a77fb357e72300e137c2b182f2e
Your Name <you@example.com>
2023-04-04 13:26:16 +0300

    Aide vtori opit

--- MedianOfTwoSortedArrays/MedianOfTwoSortedArrays/Program.cs
+++ MedianOfTwoSortedArrays/MedianOfTwoSortedArrays/Program.cs
@@ -11,26 +11,53 @@ namespace MedianOfTwoSortedArrays
     {
         static void Main(string[] args)
         {
-            int[] num1 = { 1, 2 };
-            int[] num2 = { 3, 4};
-            FindMedianSortedArrays(num1, num2);
+            int[] num1 = Array.Empty<int>();
+            int[] num2 = { 1};
+            Console.WriteLine(FindMedianSortedArrays(num1, num2));
         }
         public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
         {
-            int i;
-            int[] newArray = new int[nums1.Length + nums2.Length];
-            Array.Copy(nums1, newArray, nums1.Length);
-            Array.Copy(nums2, 0, newArray, nums1.Length, nums2.Length);
-
-            if(newArray.Length % 2 == 0)
+            if(nums1 == null && nums2 == null)
             {
-                Console.WriteLine("Cheten");
+                return 0;
             }
             else
             {
-                Console.WriteLine("Necheten");
+                double[] newArray = new double[nums1.Length + nums2.Length];
+                Array.Copy(nums1, newArray, nums1.Length);
+                Array.Copy(nums2, 0, newArray, nums1.Length, nums2.Length);
+                double elementOne = newArray[(newArray.Length / 2) - 1];
+                double elementTwo = newArray[(newArray.Length / 2)];
+
+                if (newArray.Length <= 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    if (newArray.Length % 2 == 0)
+                    {
+                        if (elementOne == 0 || elementTwo == 0)
+                        {
+                            return 0;
+                        }
+                        return (elementOne + elementTwo) / 2;
+                    }
+                    else if (newArray.Length == 1)
+                    {
+                        return newArray[0];
+                    }
+                    else
+                    {
+                        elementOne = (newArray.Length / 2) + 1;
+                        if (elementOne == 0)
+                        {
+                            return 0;
+                        }
+                        return elementOne;
+                    }
+                }
             }
-            return 1;
         }
     }
 }
